322598a07df510a9923f777fb273f4502cf131a4
karlkeefer <karl@karlkeefer.com>
2016-10-15 14:59:54 -0700

    basic site generator

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+"use strict";
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    livereload: true, // overridden to false for `grunt build`
+    jade: {
+      compile: {
+        options: {
+          data: {
+            debug: false,
+            livereload: function() {
+              return grunt.config.data.livereload;
+            }
+          }
+        },
+        files: [{
+          expand: true,
+          cwd: "pages",
+          src: ["**/*.jade"],
+          dest: "output",
+          ext: ".html"
+        }]
+      }
+    },
+    watch: {
+      jade: {
+        options: {
+          livereload: true
+        },
+        files: [
+          '**/*.jade'
+        ],
+        tasks: ['jade:compile']
+      },
+      clientLess: {
+        options: {
+          livereload: true
+        },
+        files: [
+          '**/*.less',
+        ],
+        tasks: ['less']
+      },
+    },
+    less: {
+      options: {
+        compress: true
+      },
+      layouts: {
+        files: {
+          'output/css/main.css': [
+            'less/main.less'
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+
+  // regular watch for local dev
+  grunt.registerTask('default', ['jade', 'less', 'watch']);
+
+  // disable livereload for real builds
+  grunt.registerTask('noLiveReload', 'swap livereload var', function() {
+    grunt.config.data.livereload = false;
+  });
+  grunt.registerTask('build', ['noLiveReload', 'jade', 'less']);
+};
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # czs
 Site generator for crazysauce.com
+
+### Setup and iterate
+```bash
+npm install -g grunt && npm install && grunt watch
+# in another terminal
+cd output && python -m SimpleHTTPServer
+# visit http://localhost:8000/ in your browser
+```
+
+### Deployment
+```bash
+grunt build && rsync -av --delete ./output/* booyah:/home/karlkeefer/public/crazysauce.com/public/
+```
